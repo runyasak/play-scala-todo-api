db646758145c1ee7e71369448e79dc09fa836a9c
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2021-12-11 21:02:04 +0700

    feat: implement Ok content on TodoListController

--- app/controllers/TodoListController.scala
+++ app/controllers/TodoListController.scala
@@ -3,16 +3,27 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+
 import scala.collection.mutable.ListBuffer
 import models._
 
 @Singleton
-class TodoController @Inject() (val controllerComponents: ControllerComponents)
-    extends BaseController {
+class TodoListController @Inject() (
+    val controllerComponents: ControllerComponents
+) extends BaseController {
 
   private val todoList = new ListBuffer[TodoListItem]()
   todoList += TodoListItem(1, "Buy new laptop", true)
   todoList += TodoListItem(2, "Call mom", false)
 
-  def getAll(): Action[AnyContent] = Action { NoContent }
+  implicit val todoItemJsonFormat = Json.format[TodoListItem]
+
+  def getAll(): Action[AnyContent] = Action {
+    if (todoList.isEmpty) {
+      NoContent
+    } else {
+      Ok(Json.toJson(todoList))
+    }
+  }
 }
