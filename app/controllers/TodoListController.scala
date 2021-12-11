4a263e376fe1087d2ba7ed8cd454b808b8d2ba70
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2021-12-11 20:48:50 +0700

    feat: add /todos with getAll function

--- app/controllers/TodoListController.scala
+++ app/controllers/TodoListController.scala
@@ -4,10 +4,9 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
-
 @Singleton
-class TodoController @Inject()(val controllerComponents: ControllerComponents) 
-	extends BaseController {
-	
-def getAll(): Action[AnyContent] = Action {NoContent}
+class TodoController @Inject() (val controllerComponents: ControllerComponents)
+    extends BaseController {
+
+  def getAll(): Action[AnyContent] = Action { NoContent }
 }
